6faef96c2422ad7d7f73e18a2b258b6b84c64c25
Jamie Ynoñan <jamiea31@gmail.com>
2016-02-17 01:52:34 -0500

    fixed ArrayH method get value

--- src/ArrayH.php
+++ src/ArrayH.php
@@ -32,6 +32,17 @@ class ArrayH
         }
         if (is_array($array) && array_key_exists($key, $array)) {
             return $array[$key];
+        }
+
+        if (($pos = strrpos($key, '.')) !== false) {
+            $array = static::getValue($array, substr($key, 0, $pos), $default);
+            $key = substr($key, $pos + 1);
+        }
+
+        if (is_object($array)) {
+            return $array->$key;
+        } elseif (is_array($array)) {
+            return array_key_exists($key, $array) ? $array[$key] : $default;
         } else {
             return $default;
         }
